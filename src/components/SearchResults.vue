782354abf64a86113c9ad149b2e1a84aa4bd31b6
jordanhudgens <jordan@devcamp.com>
2018-05-22 10:00:54 -0500

    Added search endpoint

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -9,7 +9,36 @@ export default {
   name: 'SearchResults',
   data() {
     return {
+      query: null,
+      results: [],
+      gettingResults: true,
     }
+  },
+  beforeMount() {
+    this.query = this.$route.params.query;
+    this.getResults(this.query);
+  },
+  watch: {
+    '$route'(to, from) {
+      console.log('watcher...');
+    }
+  },
+  methods: {
+    getResults(q) {
+      this.results = [];
+      console.log(q);
+
+      // axios
+      //   .get("https://api.dailysmarty.com/search", { params: { q } })
+      //   .then(response => {
+      //     this.results.push(...response.data.posts);
+      //     this.gettingResults = false;
+      //     console.log(response.data.posts);
+      //   })
+      //   .catch(error => {
+      //     console.log(error);
+      //   });
+    },
   }
 }
 </script>
--- src/components/shared/Navigation.vue
+++ src/components/shared/Navigation.vue
@@ -4,9 +4,13 @@
       <a href="#" class="toggle" id="navHamburger">
         <i class="fas fa-bars"></i>
       </a>
-      <a href="#" class="brand">MyName</a>
-      <div class="left">
-        <input type="text" placeholder="Search">
+
+      <router-link class="brand" :to="{ name: 'Homepage' }">
+        <img src="@/assets/logo.png" alt="">
+      </router-link>
+
+      <div class="navSearchBar">
+        <input type="text" :value="query" :placeholder="searchPlaceholder" @keyup.enter="submitQuery">
       </div>
       <div class="right">
         <a v-for="navLink in loggedInNavLinks" :key="navLink.linkname" href="#" class="link">
@@ -28,6 +32,8 @@ export default {
   },
   data() {
     return {
+      query: null,
+      searchPlaceholder: `\uf002 Search Devos`,
       loggedInNavLinks: [
         { linkName: 'Categories', linkIcon: 'navIcon fas fa-map-signs' },
         { linkName: 'Popular', linkIcon: 'navIcon fas fa-chart-line' },
@@ -35,6 +41,20 @@ export default {
         { linkName: 'Publish', linkIcon: 'navIcon fas fa-plus-square' },
       ]
     }
+  },
+  methods: {
+    submitQuery(evt) {
+      this.query = evt.target.value;
+
+      console.log(this.$router);
+
+      // this.$router.history.current = null;
+
+      this.$router.push({
+        name: "SearchResults",
+        params: { query: evt.target.value }
+      });
+    },
   }
 }
 </script>
@@ -66,6 +86,8 @@ export default {
   text-decoration: none;
   color: #239a87;
   background-color: white;
+  font-size: 1.2rem;
+  border-bottom: transparent solid 1px;
 }
 
 .navbar .link:hover,
@@ -75,7 +97,7 @@ export default {
 }
 
 .navbar .brand {
-  width: 10em;
+  width: 15em;
   text-align: center;
   padding: 1em;
   color: white;
@@ -85,10 +107,43 @@ export default {
   text-decoration: none;
 }
 
+.navSearchBar {
+  font-family: "Font Awesome\ 5 Free", Helvetica, sans-serif;
+}
+
+.navSearchBar input {
+  width: 75%;
+  padding-left: 21px;
+}
+
 .navbar .toggle {
   padding: 1em;
   color: #223047;
   vertical-align: middle;
+}
+
+.brand img {
+  width: 100%;
+}
+
+.navSearchBar input[type="text"] {
+  font-size: 1.3em;
+  font-family: "Font Awesome\ 5 Free", Helvetica, sans-serif;
+  color: #585858;
+  border-top: 1px solid transparent;
+  border-right: 1px solid transparent;
+  border-left: 1px solid transparent;
+  border-bottom: 1px solid transparent;
+}
+
+.navSearchBar input[type="text"]::placeholer {
+  font-family: "Font Awesome\ 5 Free", Helvetica, sans-serif;
+  color: #a4a4a4;
+}
+
+.navSearchBar input[type="text"]:focus {
+  outline: none;
+  border-bottom: 1px solid #239a87;
 }
 
 @media (min-width: 48em) {
@@ -97,7 +152,7 @@ export default {
     display: -ms-flexbox;
     display: flex;
   }
-  .navbar .left,
+  .navbar .navSearchBar,
   .navbar .right {
     display: -webkit-box;
     display: -ms-flexbox;
