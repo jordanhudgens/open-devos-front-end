cd45a3497f9d4d379b2da410985d9c620120feae
jordanhudgens <jordan@devcamp.com>
2018-05-22 16:49:23 -0500

    Built out initial login component

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Login</h2>
+
+    <form @submit.prevent="login">
+      <input v-model="email" type="email" placeholder="Email address" required autofocus>
+      <input v-model="password" type="password" placeholder="Password" required autofocus>
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.email);
+      console.log(this.password);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Homepage from "@/components/Homepage";
 import SearchResults from "@/components/SearchResults";
+import Login from "@/components/auth/Login";
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: "/search-results",
       name: "SearchResults",
       component: SearchResults
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
     }
   ]
 });
