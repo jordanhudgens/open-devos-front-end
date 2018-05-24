847025530ccd0076d21dd260be8da7359dad6e85
jordanhudgens <jordan@devcamp.com>
2018-05-24 00:09:25 -0500

    Added the category api call

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,14 +1,36 @@
 <template>
   <div>
     <h1>Categories</h1>
+
+    <div v-for="category in categories" :key="category.id">{{ category.title }}</div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'Categories',
   data() {
     return {
+      categoryApiUrl: 'https://open-devos-api.herokuapp.com/topics',
+      categories: []
+    }
+  },
+  beforeMount() {
+    this.getCategories();
+  },
+  methods: {
+    getCategories() {
+      axios
+        .get(this.categoryApiUrl)
+        .then(response => {
+          this.categories.push(...response.data);
+          console.log(this.categories);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 }
