ea306b3715dbfd1c99a159c3764d4c30df44b939
jordanhudgens <jordan@devcamp.com>
2018-06-02 23:12:57 -0500

    Updated server changes

--- server.js
+++ server.js
@@ -2,14 +2,7 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + "/dist/"), function(req, res, next) {
-  if (!req.secure) {
-    var secureUrl = "https://" + req.headers["host"] + req.url;
-    res.writeHead(301, { Location: secureUrl });
-    res.end();
-  }
-  next();
-});
+app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
   res.sendFile(__dirname + "/dist/index.html");
 });
