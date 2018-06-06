4e7728242e178c710449fcb143512d907ecd1f95
jordanhudgens <jordan@devcamp.com>
2018-06-05 20:24:51 -0500

    Injected router for the ga tag

--- src/components/analytics/GoogleAnalytics.vue
+++ src/components/analytics/GoogleAnalytics.vue
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueAnalytics from 'vue-analytics';
-import routers from '@/router';
+import router from '@/router';
 
 Vue.use(VueAnalytics, {
   id: 'UA-120438011-1',
