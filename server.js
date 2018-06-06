c9a18bea384f77235a3d7a93253de8d0904b72c8
jordanhudgens <jordan@devcamp.com>
2018-06-06 09:36:50 -0500

    Updated server js settings

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.enable("trust proxy");
-app.use(express.static(__dirname + "/dist/"), function(req, res, next) {
+
+app.use(function(req, res, next) {
+  console.log("inside app use");
+
   if (req.protocol !== "https") {
+    console.log("inside conditional");
     var secureUrl = "https://" + req.headers["host"] + req.url;
     res.writeHead(301, { Location: secureUrl });
     res.end();
   }
   next();
 });
+
+app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
+  console.log("inside get");
   res.sendFile(__dirname + "/dist/index.html");
 });
 app.listen(port);
