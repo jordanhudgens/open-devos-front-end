59bb36de2e0580b3979f5161256a816dd63b1a2d
jordanhudgens <jordan@devcamp.com>
2018-06-10 00:05:09 -0600

    Built for production

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,23 +4,41 @@
 
     <h2>{{ profileSlug }}</h2>
 
-    <!-- <div v-for="plan in profilePlans" :key="plan.id">
-
-                  </div> -->
+    <div v-for="plan in profilePlans" :key="plan.id">
+      <pre>{{ plan }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Profile',
   data() {
     return {
-      profilePlans: null,
+      profilePlans: [],
       profileSlug: null,
     }
   },
   created() {
-    this.profileSlug = this.$route.params.user_slug
+    this.profileSlug = this.$route.params.user_slug;
+    this.getResults(this.profileSlug);
+  },
+  methods: {
+    getResults(user_slug) {
+      this.results = [];
+
+      axios
+        .get(`https://open-devos-api.herokuapp.com/profile/${user_slug}`)
+        .then(response => {
+          console.log(response.data.plans)
+          this.profilePlans.push(...response.data.plans);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
   }
 }
 </script>
