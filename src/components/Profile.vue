9fc789879f332261ef2671859a42272dfa90dc46
jordanhudgens <jordan@devcamp.com>
2018-07-09 12:13:41 -0600

    Implemented profile design by using the ThumbCards component

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    <h1>Profile</h1>
+    <h2>Plans published by {{ profileSlug }}</h2>
 
-    <h2>{{ profileSlug }}</h2>
-
-    <div v-for="plan in profilePlans" :key="plan.id">
-      <pre>{{ plan }}</pre>
-    </div>
+    <ThumbCards :collection="plans" routeName="PlanDetail" />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ThumbCards from '@/components/shared/ThumbCards';
 
 export default {
   name: 'Profile',
   data() {
     return {
-      profilePlans: [],
+      plans: [],
       profileSlug: null,
     }
   },
@@ -25,15 +22,16 @@ export default {
     this.profileSlug = this.$route.params.user_slug;
     this.getResults(this.profileSlug);
   },
+  components: {
+    ThumbCards
+  },
   methods: {
     getResults(user_slug) {
-      this.results = [];
-
       axios
         .get(`https://open-devos-api.herokuapp.com/profile/${user_slug}`)
         .then(response => {
           console.log(response.data.plans)
-          this.profilePlans.push(...response.data.plans);
+          this.plans.push(...response.data.plans);
         })
         .catch(error => {
           console.log(error);
