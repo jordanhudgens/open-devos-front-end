639ec9d9fedc235adb818d1f5a5b2f9690059308
jordanhudgens <jordan@devcamp.com>
2018-09-26 18:13:52 -0500

    Fixed indentation issue with the file selector

--- src/components/shared/FileSelect.vue
+++ src/components/shared/FileSelect.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="file-select-wrapper">
-  <label class="file-select">
-    <div class="select-button">
-      <span v-if="value">Selected File: {{value.name}}</span>
-      <span v-else>Select File</span>
-    </div>
-    <input type="file" @change="handleFileChange" />
-  </label>
+    <label class="file-select">
+      <div class="select-button">
+        <span v-if="value">Selected File: {{value.name}}</span>
+        <span v-else>Select File</span>
+      </div>
+      <input type="file" @change="handleFileChange" />
+    </label>
   </div>
 </template>
 
