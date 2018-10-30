7ce2889597361485eaa0853cf273cac6fa1994c1
jordanhudgens <jordan@devcamp.com>
2018-10-29 19:19:13 -0500

    Updated the devo manager to now get the plan and set it before passing it to the child devo form component

--- src/components/devos/DevoManager.vue
+++ src/components/devos/DevoManager.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>{{ formTitle }}</h1>
-    <DevoForm :devoToEdit.sync="devo" :planId="devo.plan.id" @new="addToDevos" @update="updateDevoList" />
+    <DevoForm :devoToEdit.sync="devo" :planId="plan.id" @new="addToDevos" @update="updateDevoList" />
   </div>
 </template>
 
@@ -9,12 +9,6 @@
 import axios from 'axios';
 import { mapGetters } from "vuex";
 import DevoForm from '@/components/devos/DevoForm';
-
-// TODOs
-// Get the plan id
-// It's needed because the devo object is empty for new devos
-// You now have access to it in the route plan_slug
-
 
 export default {
   name: 'DevoManager',
@@ -26,17 +20,24 @@ export default {
         name: null,
         slug: this.$route.params.devo_slug,
         content: null,
-        plan: null,
         featuredImage: null
       },
+      plan: {},
       devoApiUrl: 'https://open-devos-api.herokuapp.com/devos',
+      planApiUrl: `https://open-devos-api.herokuapp.com/plans/${this.$route.params.plan_slug}`
     }
+  },
+
+  components: {
+    DevoForm
   },
 
   beforeMount() {
     if (this.devo.slug !== 'new') {
       this.getDevoDetails();
     }
+
+    this.getPlan();
   },
 
   computed: {
@@ -63,14 +64,24 @@ export default {
       // Combine these
     },
 
+    getPlan() {
+      axios
+        .get(this.planApiUrl)
+        .then(response => {
+          this.plan = response.data.plan;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
     getDevoDetails() {
       axios
         .get(`${this.devoApiUrl}/${this.devo.slug}`)
         .then(response => {
-          console.log("Response", response);
+          console.log("Response from getDevoDetails", response);
           this.devo.id = response.data.devo.id;
           this.devo.name = response.data.devo.title;
-          this.devo.plan = response.data.devo.plan;
           this.devo.featuredImage = response.data.devo.featured_image;
           this.devo.content = sanitizeHtml(response.data.devo.content, {
             allowedTags: sanitizeHtml.defaults.allowedTags.concat(['h1', 'h2', 'img', 'div', 'a']),
